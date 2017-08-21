aa8da0e8c9e0331c1e7649f26c834d181f8dd414
rijpma <auke.rijpma@gmail.com>
2017-08-21 14:24:33 +0200

    docs for uriref

--- R/convert.R
+++ R/convert.R
@@ -18,12 +18,25 @@ literal = function(string, datatype = "xsd:string"){
     }
 }
 
-#' Create uri
+# todo: check for missing / or # and insert, maybe based on default
+#' Create URI
+#' 
+#' \code{uriref} creates a uri with nquad-angle brackets and any non-
+#' safe characters are %-encoded or replaced by underscores.
+#' 
+#' @param string The value to be converted to a URI.
+#' @param base The base of URI.
+#' @param path Further path of URI
+#' @examples
+#' uriref('rainy', base = 'http://www.weather.com/', path = 'weather/')
 uriref = function(string, base, path = ''){
+    to_replace = "[ %+&]"
+
     ifelse(is.na(string), NA, 
     paste0("<", base, path, urltools::url_encode(gsub(to_replace, "_", string)), ">"))
 }
 
+#' Create blank node
 bnode = function(n = 1){
     replicate(n, paste0("_:N", gsub("-", "", uuid::UUIDgenerate())))
      # N in nq serialisation to make NCName compliant-ish
