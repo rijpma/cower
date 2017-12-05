1250d55470f663a9a877c646b736652bcb3c9c21
rijpma <auke.rijpma@gmail.com>
2017-12-05 13:47:44 +0100

    expand prefixes in tableSchema columns

--- R/schema.R
+++ R/schema.R
@@ -17,9 +17,27 @@ read_json_schema(jsonpath){
     return(schema_list)
 }
 
-expand_prefixes(schema_list){
+expand_prefixes = function(schema_list){
     # take schema_list and expend prefixes in tableSchema based on namespaces
-    return(expanded_schema_list)
+    
+    # add xsd: if no prefix in datatype
+    datatypes = schema_list$tableSchema$columns$datatype
+    schema_list$tableSchema$columns$datatype = ifelse(grepl(":", datatypes), 
+        datatypes, paste0("xsd:", datatypes))
+
+    namespaces = unlist(schema_list$`@context`[[3]]) # is this always in position 3?
+    for (i in 1:length(namespaces)){
+        namespace_pattern = paste0("^", names(namespaces[i]), ":")
+
+        for (j in 1:ncol(schema_list$tableSchema$columns)){
+            schema_list$tableSchema$columns[, j] = gsub(pattern = namespace_pattern, 
+                replacement = namespaces[i], 
+                x = schema_list$tableSchema$columns[, j])
+        }        
+    }
+    # also aboutUrl and elsewhere in schema_list. 
+
+    return(schema_list)
 }
 
 split_schema_uris(schema_list){
