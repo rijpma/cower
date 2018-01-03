faf76a9ee543b1fff9afb24ec0f44408ea805f50
rijpma <auke.rijpma@gmail.com>
2018-01-03 23:37:05 +0100

    separate hash function

--- R/graph_names.R
+++ R/graph_names.R
@@ -18,16 +18,20 @@
 graph_names = function(path_to_csv, 
     base = "https://iisg.amsterdam/resource/hsn/", 
     sub_path = ""){
-    filehash = git2r::hashfile(path_to_csv)
+
     now = format(Sys.time(), "%Y-%m-%dT%H:%M")
     graphs = c('assertion', 'nanopublication', 'provenance', 'pubinfo', 'resource')
 
     # better: use uriref
     nanopub = paste0("<", base, sub_path, graphs, "/", 
-         substring(filehash, 1, 8), "/", now, ">")
+         githash(path_to_csv)['short'], "/", now, ">")
 
     names(nanopub) = graphs
 
     return(nanopub)
 }
 
+githash = function(path_to_csv){
+    filehash = git2r::hashfile(path_to_csv)
+    return(c(full = filehash, short = substring(filehash, 1, 8)))
+}
