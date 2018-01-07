83235c930fdada216a7297e5fa09d5af371bf29a
rijpma <auke.rijpma@gmail.com>
2018-01-07 18:59:47 +0100

    update cower function

--- R/cower.R
+++ R/cower.R
@@ -1,9 +1,32 @@
-cower = function(csv_path, json_path, nquad_path, compress = TRUE){
+cower = function(csv_path, json_path, nquad_path, 
+    compress = TRUE){
 
     schema_list = read_json_schema(json_path)
     dat = data.table::fread(csv_path)
-
     namespaces = get_namespaces(schema_list) # get_namespaces does not function on expanded schema_list
+    filehash = githash(csv_path)
+
+    named_graphs = graph_names(filehash['short'], base = schema_list$`@context`[[2]]$`@base`)
+
+    nanopub = nanopublication(schema_list = schema_list, 
+        graph_names = named_graphs,
+        namespaces = namespaces,
+        hashes = filehash)
+
+    nqwrite(dat = nanopub, 
+        nquadpath = nquad_path, 
+        append = FALSE,
+        compress = compress)
+
+    mg = metagraph(schema_list, named_graphs)
+
+    if (compress) {
+        outfile = gzfile(nquad_path, open = "a")
+    } else {
+        outfile = file(nquad_path, open = "a")
+    }
+    writeLines(text = mg, con = outfile)
+    close(outfile)
 
     schema_list = prep_table_schema(schema_list = schema_list)
     schema_list = expand_prefixes(schema_list, namespaces)
@@ -19,15 +42,6 @@ cower = function(csv_path, json_path, nquad_path, compress = TRUE){
     dat[, pred := colnames_to_predicates(schema_list = schema_list)[as.numeric(pred)]]
 
     nqwrite(dat = dat[complete.cases(dat), list(sub, pred, obj, graph)], 
-        nquadpath = nquad_path, 
-        append = FALSE,
-        compress = compress)
-
-    mg = metagraph(schema_list = schema_list, 
-        csv_path = "/Users/auke/repos/cower/test.csv-metadata.json",
-        namespaces = namespaces)
-
-    nqwrite(dat = mg, 
         nquadpath = nquad_path, 
         append = TRUE,
         compress = compress)
