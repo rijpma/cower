cef7ffbf4fd53634c8bc3b0712ef1aca8ec4bff2
rijpma <auke.rijpma@gmail.com>
2018-01-09 15:19:42 +0100

    batch size option

--- R/cower.R
+++ R/cower.R
@@ -1,8 +1,7 @@
 cower = function(csv_path, json_path, nquad_path, 
-    compress = TRUE, nrows = -1L){
+    compress = TRUE, batch_size = -1L){
 
     schema_list = read_json_schema(json_path)
-    dat = data.table::fread(csv_path, nrows = nrows)
     namespaces = get_namespaces(schema_list) # get_namespaces does not function on expanded schema_list
     filehash = githash(csv_path)
 
@@ -33,17 +32,32 @@ cower = function(csv_path, json_path, nquad_path,
     schema_list = expand_prefixes(schema_list, namespaces)
     schema_list$tableSchema$columns = as.data.frame(schema_list$tableSchema$columns, stringsAsFactors = F)
 
-    convert(dat = dat, 
-        schema_list = schema_list)
-    
-    dat[, graph := named_graphs['assertion']]
-    
-    dat = triples(dat, schema_list)
-    
-    dat[, pred := colnames_to_predicates(schema_list = schema_list)[as.numeric(pred)]]
+    done = 0
+    current_batch_size = batch_size
+    header = data.table::fread(csv_path, header = TRUE, nrow = 1)
+    while (current_batch_size == batch_size){
+        batch = data.table::fread(csv_path,
+            nrows = batch_size, skip = done + 1,
+            header = FALSE)
+        current_batch_size = nrow(batch)
 
-    nqwrite(dat = dat[complete.cases(dat), list(sub, pred, obj, graph)], 
-        nquadpath = nquad_path, 
-        append = TRUE,
-        compress = compress)
+        setnames(batch, names(batch), names(header))
+
+        convert(dat = batch,
+            schema_list = schema_list)
+
+        batch[, graph := named_graphs['assertion']]
+
+        batch = triples(batch, schema_list)
+
+        batch[, pred := colnames_to_predicates(schema_list = schema_list)[as.numeric(pred)]]
+
+        nqwrite(dat = batch[complete.cases(batch), list(sub, pred, obj, graph)],
+            nquadpath = nquad_path,
+            append = TRUE,
+            compress = compress)
+
+        done = done + current_batch_size
+        cat("Done ", done, "rows.\n")
+    }
 }
